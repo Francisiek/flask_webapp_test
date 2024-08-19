044cf97c05a379fd3f5fa5b408098fcb211d00ad
Franc <franc@localhost.localdomain>
2024-08-19 18:43:49 +0200

    first db tries

--- blog.py
+++ blog.py
@@ -1,1 +1,9 @@
-from webapp import app
+import sqlalchemy as sqa
+import sqlalchemy.orm as sqo
+from webapp import app, db
+from webapp.models import User, Post
+
+
+@app.shell_context_processor
+def shell_context():
+    return {'sqa': sqa, 'sqo': sqo, 'app': app, 'db': db, 'User': User, 'Post': Post}
--- webapp/models.py
+++ webapp/models.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from datetime import datetime, timezone
 import sqlalchemy as SA
 import sqlalchemy.orm as SO
 from webapp import db
@@ -9,5 +10,20 @@ class User(db.Model):
     email:      SO.Mapped[str] = SO.mapped_column(SA.String(128), index=True, unique=True)
     hashed_pass:SO.Mapped[Optional[str]] = SO.mapped_column(SA.String(256))
 
+    posts:      SO.WriteOnlyMapped['Post'] = SO.relationship(back_populates='author')
+
     def __repr__(self):
         return f'<User {self.username}>'
+    
+class Post(db.Model):
+    id:         SO.Mapped[int] = SO.mapped_column(primary_key=True)
+    user_id:    SO.Mapped[int] = SO.mapped_column(SA.ForeignKey(User.id), index=True)
+    timestamp:  SO.Mapped[datetime] = SO.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
+    title:      SO.Mapped[str] = SO.mapped_column(SA.String(64), index=True)
+    body:       SO.Mapped[str] = SO.mapped_column(SA.String(4096))
+
+    author:     SO.Mapped[User] = SO.relationship(back_populates='posts')
+
+    def __repr__(self):
+        return f'<Post {self.title} by {self.user_id}'
+    
